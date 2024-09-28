996f6414f5c5c942378108a6ed1ce19a9dd3325a
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-09-28 17:30:23 +0530

    Sending message example in concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,52 +1,81 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
+
+type Message struct {
+	from    string
+	payload string
+}
+
+type Server struct {
+	msgch chan Message
+}
+
+func (s *Server) startAndListen() {
+	for {
+		msg := <-s.msgch
+		fmt.Printf("Message received from %s : %s\n", msg.from, msg.payload)
+	}
+}
+
+func (s *Server) sendMessage(payload string) {
+	fmt.Println("Sending message......")
+	msg := Message{
+		from:    "John Doe",
+		payload: payload,
+	}
+	s.msgch <- msg
+}
+
+func main() {
+	server := &Server{
+		msgch: make(chan Message),
+	}
+	go server.startAndListen()
+	server.sendMessage("Hello from Go!")
+}
 
 // Synchronous and Asynchronous staff
-func main() {
-	userId := 10
-	now := time.Now()
-	respch := make(chan string, 128)
-	var wg sync.WaitGroup
+// func main() {
+// 	userId := 10
+// 	now := time.Now()
+// 	respch := make(chan string, 128)
+// 	var wg sync.WaitGroup
 
-	go fetchUser(userId, respch, &wg)
-	go fetchUserRecommendations(userId, respch, &wg)
-	go fetchUserLikes(userId, respch, &wg)
-	wg.Add(3)
-	wg.Wait()
-	close(respch)
+// 	go fetchUser(userId, respch, &wg)
+// 	go fetchUserRecommendations(userId, respch, &wg)
+// 	go fetchUserLikes(userId, respch, &wg)
+// 	wg.Add(3)
+// 	wg.Wait()
+// 	close(respch)
 
-	// fmt.Println(user)
-	// fmt.Println(userRecommendations)
-	// fmt.Println(userLikes)
+// 	// fmt.Println(user)
+// 	// fmt.Println(userRecommendations)
+// 	// fmt.Println(userLikes)
 
-	for resp := range respch {
-		fmt.Println(resp)
-	}
+// 	for resp := range respch {
+// 		fmt.Println(resp)
+// 	}
 
-	fmt.Println(time.Since(now)) //Output = 251ms without goroutines
-	//Output = 121 ms after goroutines
+// 	fmt.Println(time.Since(now)) //Output = 251ms without goroutines
+// 	//Output = 121 ms after goroutines
 
-}
+// }
 
-func fetchUser(userId int, respch chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(80 * time.Millisecond)
-	respch <- "user"
-}
+// func fetchUser(userId int, respch chan string, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	time.Sleep(80 * time.Millisecond)
+// 	respch <- "user"
+// }
 
-func fetchUserRecommendations(userId int, respch chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(120 * time.Millisecond)
-	respch <- "user recommendations"
-}
+// func fetchUserRecommendations(userId int, respch chan string, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	time.Sleep(120 * time.Millisecond)
+// 	respch <- "user recommendations"
+// }
 
-func fetchUserLikes(userId int, respch chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(50 * time.Millisecond)
-	respch <- "user likes"
-}
+// func fetchUserLikes(userId int, respch chan string, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	time.Sleep(50 * time.Millisecond)
+// 	respch <- "user likes"
+// }
