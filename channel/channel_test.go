5586d18c1ae5c964448f50a87e76c65d2c5f2158
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-09-28 12:07:57 +0530

    channels completed

---
+++ channel/channel_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAddUser(t *testing.T) {
+	// server := NewServer()
+	// server.Start()
+
+	// for i := 0; i < 10; i++ {
+	// go func(i int) {
+	//server.userch <- fmt.Sprintf("User_%d", i)
+	// }(i)
+	// }
+	// fmt.Println("the loop is done")
+}
--- channel/main.go
+++ channel/main.go
@@ -1,12 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Server struct {
+	users map[string]string
+	//userch chan string
+	quitch chan struct{}
+}
+
+func NewServer() *Server {
+	return &Server{
+		users: make(map[string]string),
+		//userch: make(chan string),
+		quitch: make(chan struct{}),
+	}
+}
+
+// func (s *Server) Start() {
+// 	go s.loop()
+// }
+
+//func (s *Server) loop() {
+//	for {
+// user := <-s.userch
+// s.users[user] = user
+// fmt.Printf("Adding new user %s\n", user)
+// 		select {
+// 		case msg := <-s.userch:
+// 			fmt.Println(msg)
+// 		case <-s.quitch:
+// 			return
+// 		}
+// 	}
+// }
 
 func main() {
 	userch := make(chan string, 1)
 
-	// userch <- "BOB"
-	// userch <- "ALICE"
+	userch <- "BOB"
+	//userch <- "ALICE"
 	// //userch <- "JOHN" // waiting for the consume of the channel
 
 	// for unbuffered channels
@@ -16,17 +51,33 @@ func main() {
 
 	// user := <-userch
 	// fmt.Println(user)
-	senddMessage(userch)
-	readMessage(userch)
+	// senddMessage(userch)
+	// readMessage(userch)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		close(userch)
+	}()
+
+	username, ok := <-userch
+	if !ok {
+		fmt.Println("user chan closed returning")
+		return
+	}
+	fmt.Printf("Username: %s\n", username)
+
+	if len(username) == 0 {
+		panic("User not good")
+	}
 }
 
 // Protecting channels
 
-func senddMessage(msgch chan<- string) {
-	msgch <- "Hello!"
-}
+// func senddMessage(msgch chan<- string) {
+// 	msgch <- "Hello!"
+// }
 
-func readMessage(msgch <-chan string) {
-	msg := <-msgch
-	fmt.Println(msg)
-}
+// func readMessage(msgch <-chan string) {
+// 	msg := <-msgch
+// 	fmt.Println(msg)
+// }
