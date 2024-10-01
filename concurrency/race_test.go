c6093826b7f70b0ac4eebaed94061e0e4bfa1e06
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-10-01 23:17:53 +0530

    Race conditions

---
+++ concurrency/race_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestDataRaceConditions(t *testing.T) {
+	var state int32
+	var mu sync.RWMutex
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			mu.Lock()
+			state += int32(i)
+			mu.Unlock()
+		}(i)
+	}
+}
